36a42b96a54a59f9430efbdc7efdf65b342827e4
Kofi Frempong <kofifrempong1@gmail.com>
2024-10-01 19:32:52 -0600

    commit message

---
+++ README.md
@@ -0,0 +1,32 @@
+
+
+### Description
+
+A portfolio to showcase my past work as a developer/designers. 
+
+
+### Features
+
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+- Edit Content From One Place
+
+### Setup
+
+Get the code
+
+<pre>git clone </pre>
+ 
+Install required dependencies
+
+<pre>npm install</pre>
+
+
+Start the server
+
+<pre>npm start</pre>
+
+### More
+Modify pages content in  `src/content_option.js`.
---
+++ src/content_option.js
@@ -0,0 +1,89 @@
+const logotext = "Kofi";
+const meta = {
+    title: "Kofi Frempong",
+    description: "I’m Kofi Frempong _ developer, seeking for a new challenge",
+};
+
+const introdata = {
+    title: "I’m Kofi Frempong",
+    animated: {
+        first: "Developer"
+       
+    },
+    description: "",
+
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "Computer Science student at the University of Calgary. Academic and personal projects in machine learning, back-end development and front-end development. Proficiency in multiple programming languages and good communication, organizational, troubleshooting and teamwork-related skills.",
+};
+const worktimeline = [{
+      }
+];
+
+const skills = [{/*
+        name: "Python",
+        value: 80,
+    },
+    {
+        name: "Django",
+        value: 40,
+    },
+    {
+        name: "PHP",
+        value: 80,
+    },
+    {
+        name: "Laravel",
+        value: 80,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 70,
+   */  },
+];
+
+const services = [{ },
+];
+
+const dataportfolio = [{
+        img: require('./assets/images/Resume.png'),
+        link: "https://drive.google.com/file/d/1ErFO_rHp0o7QxXCYuuZh8vXPxClDL2vz/view?usp=drive_link",
+        
+},];
+
+const contactConfig = {
+    YOUR_EMAIL: "kofi.frempong@ucalgary.ca",
+    YOUR_FONE: "",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_pivrb2o",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/kofifrempong",
+    linkedin: "https://www.linkedin.com/in/kofi-frempong-841580295",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
